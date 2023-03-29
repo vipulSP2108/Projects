b7a46d3f0bb1c048c6f537acf3ca6ec16f08b8b9
Vipul Sunil Patil <117573813+vipulSP2108@users.noreply.github.com>
2023-03-29 19:40:33 +0530

    Update main.py

--- Robospeaker/main.py
+++ Robospeaker/main.py
@@ -1,1 +1,11 @@
-print('Hi! Welcome to Robospeaker')
+import os
+
+command = 'say'
+os.system(f'{command} Hi! Welcome to Robospeaker')
+os.system(f'{command} For ending the conversation print end')
+while True:
+    speak = input('say : ')
+    if speak.lower() == 'end':
+        os.system(f'{command} Bye! See you soon')
+        break
+    os.system(f'{command} speak')
